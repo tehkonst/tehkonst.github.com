aed506332aae78414ee623b26677ddfe4d29690c
tehkonst <teh.kos@gmail.com>
2012-11-20 16:23:10 +0400

    Тест кириллицы

--- source/settings.py
+++ source/settings.py
@@ -2,7 +2,18 @@
 # coding: utf-8
 
 # Base
-AUTHOR = 'Konstantin The'
+AUTHOR = 'Konstantin'
 SITENAME = u'НИОЧЕМ'
 SITEURL = 'http://tehkonst.github.com'
 TIMEZONE = 'Europe/Moscow'
+STATIC_PATHS = ['images', 'sources', ]
+DEFAULT_PAGINATION = 5
+DEFAULT_LANG = 'ru'
+
+LOCALE = "C"
+DEFAULT_DATE = (2012, 03, 02, 14, 01, 01)
+
+DATE_FORMATS = {
+    'ru': ('rus','%d.%m.%Y'),
+	'en': ('usa','%a, %d %b %Y'),
+}
